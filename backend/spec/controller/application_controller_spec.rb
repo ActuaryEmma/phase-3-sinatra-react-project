a2c691b763c3a599078d5fb5b78b06a89636940c
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-09-15 11:41:12 +0300

    tests

---
+++ backend/spec/controller/application_controller_spec.rb
@@ -0,0 +1,41 @@
+
+describe ApplicationController do
+
+    let(:author) {PoemAuthor.first}
+
+    before do
+        PoemAuthor.create(name: "William Carlos Williams",publication: "The Red Wheelbarrow", major_achievements: "Published 10 books at the age of 25 years" )
+    end
+
+    describe 'GET /poemauthor' do
+        it 'returns an array of JSON objects for all messages in the database' do
+        get '/poemauthor'
+    
+        expect(last_response.body).to include_json([
+            {name: "William Carlos Williams",publication: "The Red Wheelbarrow", major_achievements: "Published 10 books at the age of 25 years" },
+
+        ])
+        end
+    end
+
+    describe 'PATCH/poemauthor/:id' do
+        let(:params) do
+            {publication: "Money"}
+        end
+
+        it "updatesthe publication of the poemauthor in the database" do
+            expect do 
+                patch "/poemauthor/#{author.id}", params
+
+                author.reload
+            end.to change(author, :publication).to("Money")
+
+        end
+    end
+
+    describe 'DELETE/poemauthor/:id' do
+        it 'deletes the author from the database' do
+            expect {delete "/poemauthor/#{author.id}"}.to change(PoemAuthor, :count).from(1).to(0)
+        end
+    end
+end
